39e531b3ead7de13e6995b09d9c4975e7129aef4
panwyll <53861587+panwyll@users.noreply.github.com>
2026-05-06 17:41:22 +0100

    Integrate Google Tag Manager in RootLayout
    Added Google Tag Manager script to the head section.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,26 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KBQM39CV');
+          `}
+        </Script>
+      </head>
       <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KBQM39CV"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
         {children}
         <Analytics />
 
